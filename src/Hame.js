b3276ca32672eacc61ed61881a0a1cdc47c84124
Jeff <jbridges1119@gmail.com>
2022-07-29 19:31:58 -0600

    Input prop data and created States for blog

--- src/Hame.js
+++ src/Hame.js
@@ -1,11 +1,37 @@
-
+import React, { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-  return ( 
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
+  const [name,setName] = useState("mario")
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+  //doesn't mutate blogs - filter info based on a boolean value pass
+  const filterBlogs = blogs.filter((blog) => blog.author === "mario");
+
+
+  useEffect(() => {
+
+  },[name])
+
+  return (
     <div className="home">
-      <h2>Homepage</h2>
+      <BlogList blogs={blogs} handleDelete={handleDelete} title="All Blogs!" />
+      <button onClick={() => setName("luigi")} >change name</button>
+      <p>{name}</p>
     </div>
-   );
-}
- 
+  );
+};
+
 export default Home;
